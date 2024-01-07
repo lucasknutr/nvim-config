132860f6efafe5caf058c30ca9fb34862c1e4c5b
Lucas <lucasclaudinocc@gmail.com>
2024-01-07 17:10:29 -0300

    Adding new plugins

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,28 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- local mocha = require("catppuccin.palettes").get_palette "mocha"
--- colorscheme catppuccin " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha "
-
+require("nvim-options")
+require("shortcuts")
 require("lazy").setup("plugins")
-
-
--- custom shortcuts
--- copy to clipboard using ctrl+shift+c
-vim.keymap.set("v", "<C-c>", '"+y', {})
-vim.keymap.set("n", "<C-c>", '"+y', {})
--- paste from clipboard using ctrl+shift+v
-vim.keymap.set("i", "<C-v>", '<ESC>"+pa', {})
-vim.keymap.set("n", "<C-v>", '"+p', {})
-
--- Move line/selection up
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
--- Move line/selection down
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set('n', '<C-e>', ':E <CR>', { noremap = true, silent = true })
-
 
 
 
---
+++ lua/shortcuts.lua
@@ -0,0 +1,17 @@
+-- custom shortcuts
+-- copy to clipboard using ctrl+shift+c
+vim.keymap.set("v", "<C-c>", '"+y', {})
+vim.keymap.set("n", "<C-c>", '"+y', {})
+-- paste from clipboard using ctrl+shift+v
+vim.keymap.set("i", "<C-v>", '<ESC>"+pa', {})
+vim.keymap.set("n", "<C-v>", '"+p', {})
+
+-- Move line/selection up
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+-- Move line/selection down
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set('n', '<C-e>', ':E <CR>', { noremap = true, silent = true })
+
+
