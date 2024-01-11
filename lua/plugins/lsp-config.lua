65c27eaaf1154fdfead276a09717520be6ffb2a0
Lucas <lucasclaudinocc@gmail.com>
2024-01-11 11:28:10 -0300

    added lsp-configs for the languages I use

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,39 @@
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup()
+      end
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+        config = function()
+          require("mason-lspconfig").setup({
+            ensure_installed = {
+              "lua_ls",
+              "tsserver",
+              "bashls",
+              "jsonls",
+              "quick_lint_js",
+              "rust_analyzer"
+              },
+          })
+        end
+    },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        local lspconfig = require('lspconfig')
+        lspconfig.lua_ls.setup({})
+        lspconfig.tsserver.setup({})
+        lspconfig.bashls.setup({})
+        lspconfig.jsonls.setup({})
+        lspconfig.quick_lint_js.setup({})
+        lspconfig.rust_analyzer.setup({})
+        -- shortcuts
+        vim.keymap.set('n', 'N', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+      end
+    }
 }
--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -12,6 +12,13 @@ vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true
 -- Move line/selection down
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
 vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+-- Move to explore mode
 vim.keymap.set('n', '<C-e>', ':E <CR>', { noremap = true, silent = true })
 
+-- Move to terminal mode
+vim.keymap.set('n', '<C-t>', ':term <CR>', { noremap = true, silent = true })
+
+-- Exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
