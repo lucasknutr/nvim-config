72fc1ffb2d225fc4b95124aa22a898af89698dc9
Lucas <lucasclaudinocc@gmail.com>
2024-01-16 11:57:35 -0300

    added new plugins and shortcut commands

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require('toggleterm').setup({
+      size = 35,
+      open_mapping = [[<c-t>]],
+      shade_filetypes = {},
+      direction = 'vertical',
+    })
+  end
+}
