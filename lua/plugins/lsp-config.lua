38064685e808d9310d6f0133364f7ecef2b5f968
Lucas <lucasclaudinocc@gmail.com>
2024-01-23 11:02:01 -0300

    Added various autocompletion plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,39 +1,52 @@
 return {
-    {
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
-      end
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-        config = function()
-          require("mason-lspconfig").setup({
-            ensure_installed = {
-              "lua_ls",
-              "tsserver",
-              "bashls",
-              "jsonls",
-              "quick_lint_js",
-              "rust_analyzer"
-              },
-          })
-        end
-    },
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-        local lspconfig = require('lspconfig')
-        lspconfig.lua_ls.setup({})
-        lspconfig.tsserver.setup({})
-        lspconfig.bashls.setup({})
-        lspconfig.jsonls.setup({})
-        lspconfig.quick_lint_js.setup({})
-        lspconfig.rust_analyzer.setup({})
-        -- shortcuts
-        vim.keymap.set('n', 'N', vim.lsp.buf.hover, {})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-      end
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"bashls",
+					"jsonls",
+					"quick_lint_js",
+					"rust_analyzer",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.quick_lint_js.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			-- shortcuts
+			vim.keymap.set("n", "N", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,45 @@
-
-  return {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    config = function()      
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-      ensure_installed = {"lua", "javascript", "typescript", "tsx", "html", "css", "json", "yaml", "bash", "python", "go", "rust", "toml", "dockerfile", "lua", "vim", "regex", "query", "comment", "c", "cpp", "cmake", "java", "latex", "php", "ruby", "scss", "svelte", "tsx", "vue", "yaml"},
-      highlight = { enable = true },
-      indent = { enable = true },
-      })
-    end
-  }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+      auto_install = true,
+			ensure_installed = {
+				"lua",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"json",
+				"yaml",
+				"bash",
+				"python",
+				"go",
+				"rust",
+				"toml",
+				"dockerfile",
+				"lua",
+				"vim",
+				"regex",
+				"query",
+				"comment",
+				"c",
+				"cpp",
+				"cmake",
+				"java",
+				"latex",
+				"php",
+				"ruby",
+				"scss",
+				"svelte",
+				"tsx",
+				"vue",
+				"yaml",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
